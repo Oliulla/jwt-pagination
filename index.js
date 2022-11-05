3d0a785b76fb086df2e65d7162f2f07e14286e64
oli <razasm29@gmail.com>
2022-11-06 02:50:16 +0600

    finished

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
@@ -8,6 +9,7 @@ const { getAllUsers } = require('./controllers/getUsersController');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
